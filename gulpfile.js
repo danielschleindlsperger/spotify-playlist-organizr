83f7c5ec3741e6d70fc5bd222f22011cfa69fd6d
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2016-10-02 17:13:04 +0200

    fix paths in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ let elixir = require('laravel-elixir');
 
 elixir.config.assetsPath = './src';
 elixir.config.publicPath = '';
-elixir.config.css.outputFolder = 'dist/css';
-elixir.config.js.outputFolder = 'dist/js';
+elixir.config.css.outputFolder = './dist/css';
+elixir.config.js.outputFolder = './dist/js';
 
 elixir(function (mix) {
 	// app.js
-	mix.scriptsIn('src/js')
+	mix.scriptsIn('./src/js')
 		// vendor files
 		.combine([
 			'./node_modules/angular/angular.min.js',
@@ -17,22 +17,22 @@ elixir(function (mix) {
 			'./node_modules/angular-animate/angular-animate.min.js',
 			'./node_modules/angular-material/angular-material.min.js',
 			'./node_modules/lodash/lodash.min.js',
-		], 'dist/js/vendor.js');
+		], './dist/js/vendor.js');
 
 
 	// custom styles
-	mix.sass('src/styles/app.scss', 'dist/css/app.css');
+	mix.sass('./src/styles/app.scss', './dist/css/app.css');
 
 	// vendor styles
 	mix.styles([
-		'node_modules/angular-material/angular-material.min.css',
-	], 'dist/css/vendor.css');
+		'./node_modules/angular-material/angular-material.min.css',
+	], './dist/css/vendor.css');
 
 	// copy templates
-	mix.copy('src/templates', 'dist/templates');
+	mix.copy('./src/templates', './dist/templates');
 
 	// copy images
-	mix.copy('src/img', 'dist/img');
+	mix.copy('./src/img', './dist/img');
 
 	mix.browserSync({
 		proxy: false,
