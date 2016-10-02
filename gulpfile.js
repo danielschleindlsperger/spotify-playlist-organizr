edffbc72c9dd0ec26fcb259d0c9009f21b93f140
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2016-10-02 15:07:39 +0200

    add lodash dependency

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ elixir(function (mix) {
 			'./node_modules/angular-aria/angular-aria.min.js',
 			'./node_modules/angular-animate/angular-animate.min.js',
 			'./node_modules/angular-material/angular-material.min.js',
+			'./node_modules/lodash/lodash.min.js',
 		], 'dist/js/vendor.js');
 
 
